97f982e470718feba0fed38a2c1e95a216d10a4c
Ubaldo Taladriz <utaladriz@exe.cl>
2016-05-16 21:51:31 -0400

    otro

---
+++ src/main/java/cl/ucinf/clase/IntSumReducer.java
@@ -0,0 +1,26 @@
+package cl.ucinf.clase;
+
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+/**
+ * Created by utaladriz on 16-05-16.
+ */
+
+public  class IntSumReducer
+        extends Reducer<Text,IntWritable,Text,IntWritable> {
+    private IntWritable result = new IntWritable();
+
+    public void reduce(Text key, Iterable<IntWritable> values,
+                       Context context
+    ) throws IOException, InterruptedException {
+        int sum = 0;
+        for (IntWritable val : values) {
+            sum += val.get();
+        }
+        result.set(sum);
+        context.write(key, result);
+    }
+}
---
+++ src/main/java/cl/ucinf/clase/TokenizerMapper.java
@@ -0,0 +1,27 @@
+package cl.ucinf.clase;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+/**
+ * Created by utaladriz on 16-05-16.
+ */
+
+public  class TokenizerMapper
+        extends Mapper<Object, Text, Text, IntWritable>{
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(Object key, Text value, Context context
+    ) throws IOException, InterruptedException {
+        StringTokenizer itr = new StringTokenizer(value.toString());
+        while (itr.hasMoreTokens()) {
+            word.set(itr.nextToken());
+            context.write(word, one);
+        }
+    }
+}
