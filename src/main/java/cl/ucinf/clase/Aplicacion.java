3620025fafb0dbb9f7ca8d7258a848d6d816b96b
Ubaldo Taladriz <utaladriz@exe.cl>
2016-06-01 22:47:41 -0400

    first commit

--- src/main/java/cl/ucinf/clase/Aplicacion.java
+++ src/main/java/cl/ucinf/clase/Aplicacion.java
@@ -3,7 +3,7 @@ package cl.ucinf.clase;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
@@ -14,22 +14,22 @@ import org.apache.hadoop.util.GenericOptionsParser;
  * Created by utaladriz on 16-05-16.
  */
 
-public class WordCount {
+public class Aplicacion {
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
         String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
         if (otherArgs.length < 2) {
-            System.err.println("Usage: wordcount <in> [<in>...] <out>");
+            System.err.println("Usage: maximo <in> [<in>...] <out>");
             System.exit(2);
         }
-        Job job = Job.getInstance(conf, "word count");
-        job.setJarByClass(WordCount.class);
+        Job job = Job.getInstance(conf, "maximo");
+        job.setJarByClass(Aplicacion.class);
         job.setMapperClass(TokenizerMapper.class);
-        job.setCombinerClass(IntSumReducer.class);
-        job.setReducerClass(IntSumReducer.class);
+        job.setCombinerClass(DoubleReducer.class);
+        job.setReducerClass(DoubleReducer.class);
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
+        job.setOutputValueClass(DoubleWritable.class);
         for (int i = 0; i < otherArgs.length - 1; ++i) {
             FileInputFormat.addInputPath(job, new Path(otherArgs[i]));
         }
